099bf17305662fa9a6999e0eceeb50f8232e6115
Nicholas Channg <nicholas.channg@gmail.com>
2023-12-31 02:54:33 -0500

    1.5
    Getting ready for image slider.

---
+++ src/components/Slider/slider.js
@@ -0,0 +1,11 @@
+import "./slider.scss";
+
+export default function Slider() {
+  return (
+    <>
+      <section className="section slider-section">
+        <div className="container slider-container"></div>
+      </section>
+    </>
+  );
+}
