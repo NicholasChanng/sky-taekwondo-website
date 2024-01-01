cdf3394a207b4099df4bb5450b8b3fb48539ca91
AnthonyChanng <155197190+AnthonyChanng@users.noreply.github.com>
2024-01-01 18:44:28 -0500

    Anthony - 1.9
    Added schedules, can be changed if not to liking

--- src/components/Schedule/schedule.js
+++ src/components/Schedule/schedule.js
@@ -1,9 +1,17 @@
 import "./schedule.scss";
+import BRschedule from "../../assets/images/BRschedule.jpg";
+import Cschedule from "../../assets/images/Cschedule.jpg";
 
 export default function Schedule() {
   return (
     <section className="top-section section schedule-section">
-      <div className="container schedule container">Schedule</div>
+      <div className="container schedule container">Schedules</div>
+      <div className="picture1">
+        <img id="BRschedule" src={BRschedule} alt="BRschedule" />
+      </div>
+      <div className="picture2">
+        <img id="Cschedule" src={Cschedule} alt="Cschedule" />
+      </div>
     </section>
   );
 }
