964c4fba8c68f7c578c1091236d77725d1c02d84
Nicholas Channg <nicholas.channg@gmail.com>
2024-01-02 08:35:20 -0500

    1.8
    Minor changes to Park and Schedule pages.

--- src/components/Park/park.js
+++ src/components/Park/park.js
@@ -13,15 +13,16 @@ export default function Park() {
           <div className="description">
             <h1>
               Grand Master Park obtained his degree in Taekwondo from Kyunghee
-              University and holds a 7th-degree Black Belt. Additionally, while
+              University and holds a 8th-degree Black Belt. Additionally, while
               enrolled at Fairleigh Dickinson University, he pursued a Master of
               Sports Administration. He has been instructing Taekwondo for over
               25 years, and many of his students have participated in World
-              Championship tournaments. Master Park always cares for his
-              students and helps them achieve future success. Sky Taekwondo was
-              founded with the intention of making sure students will have the
-              most rewarding learning experience possible and will be able to
-              change themselves for the better.
+              Championship tournaments. <br /> <br />
+              Master Park always cares for his students and helps them achieve
+              future success. Sky Taekwondo was founded with the intention of
+              making sure students will have the most rewarding learning
+              experience possible and will be able to change themselves for the
+              better.
             </h1>
           </div>
         </div>
--- src/components/Schedule/schedule.js
+++ src/components/Schedule/schedule.js
@@ -5,13 +5,17 @@ import Cschedule from "../../assets/images/Cschedule.jpg";
 export default function Schedule() {
   return (
     <section className="top-section section schedule-section">
-      <div className="container schedule container">Schedules</div>
-      <button className="BaskingRidge">Basking Ridge Schedule</button>
-      <div className="picture1">
-        <img id="BRschedule" src={BRschedule} alt="BRschedule" />
-      </div>
-      <div className="picture2">
-        <img id="Cschedule" src={Cschedule} alt="Cschedule" />
+      <div className="container schedule-container">
+        <div className="flex schedule-flex">
+          <h1>Schedules</h1>
+          <button className="BaskingRidge">Basking Ridge Schedule</button>
+          <div className="picture1">
+            <img id="BRschedule" src={BRschedule} alt="BRschedule" />
+          </div>
+          <div className="picture2">
+            <img id="Cschedule" src={Cschedule} alt="Cschedule" />
+          </div>
+        </div>
       </div>
     </section>
   );
