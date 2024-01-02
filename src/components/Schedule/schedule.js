cfe41ab0c2c80ea91d1fd417c6d405a7b3d6ab94
AnthonyChanng <155197190+AnthonyChanng@users.noreply.github.com>
2024-01-01 22:21:42 -0500

    Anthony - 2.0
    fixed sizing of schedules, attempted adding button to show schedules

--- src/components/Schedule/schedule.js
+++ src/components/Schedule/schedule.js
@@ -6,6 +6,7 @@ export default function Schedule() {
   return (
     <section className="top-section section schedule-section">
       <div className="container schedule container">Schedules</div>
+      <button className="BaskingRidge">Basking Ridge Schedule</button>
       <div className="picture1">
         <img id="BRschedule" src={BRschedule} alt="BRschedule" />
       </div>
