a2e1231a1a56ac2995bfadd3baf4dd2dd5fda294
AnthonyChanng <155197190+AnthonyChanng@users.noreply.github.com>
2024-01-13 22:31:03 -0500

    Anthony - 2.2
    Made schedule page responsive

--- src/components/Schedule/schedule.js
+++ src/components/Schedule/schedule.js
@@ -1,17 +1,23 @@
 import "./schedule.scss";
 import BRschedule from "../../assets/images/BRschedule.jpg";
 import Cschedule from "../../assets/images/Cschedule.jpg";
+// import BRLocation from "../../assets/images/BRLocation.JPG";
 
 export default function Schedule() {
   return (
     <section className="top-section section schedule-section">
-      <div className="container schedule container">Schedules</div>
-      <button className="BaskingRidge">Basking Ridge Schedule</button>
-      <div className="picture1">
-        <img id="BRschedule" src={BRschedule} alt="BRschedule" />
-      </div>
-      <div className="picture2">
-        <img id="Cschedule" src={Cschedule} alt="Cschedule" />
+      <div className="container schedule-container">
+        Schedules
+        {/* <button className="BaskingRidge">Basking Ridge Schedule</button> */}
+        <div className="picture1">
+          <img id="BRschedule" src={BRschedule} alt="BRschedule" />
+        </div>
+        {/* <div className="BRLocation">
+        <img id="picBR" src={BRLocation} alt="BRLocation" />
+      </div> */}
+        <div className="picture2">
+          <img id="Cschedule" src={Cschedule} alt="Cschedule" />
+        </div>
       </div>
     </section>
   );
